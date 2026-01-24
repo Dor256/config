d4cee2090410cdc2e4c720724f5cf6a599b650be
Dor256 <dorb@wix.com>
2026-01-24 13:08:44 +0200

    add ghostty

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -2,11 +2,14 @@ return {
     'akinsho/toggleterm.nvim',
     version = "*",
     config = function()
+        local default_size = 15
+        local expanded_size = 40
+
         require("toggleterm").setup({
             -- Use <C-j> to open/close the terminal
             open_mapping = [[<c-j>]],
             direction = 'horizontal',
-            size = 15,
+            size = default_size,
             -- This ensures that the terminal starts in insert mode
             start_in_insert = true,
             -- Allow the <C-j> mapping to work even when you're inside the terminal
@@ -14,6 +17,17 @@ return {
             insert_mappings = true,
             persist_size = true,
         })
+
+        -- Toggle terminal size between default and expanded
+        local is_expanded = false
+        vim.keymap.set({ "n", "t" }, "<C-t>", function()
+            if is_expanded then
+                vim.cmd("resize " .. default_size)
+            else
+                vim.cmd("resize " .. expanded_size)
+            end
+            is_expanded = not is_expanded
+        end, { desc = "Toggle terminal size" })
     end
 }
 
