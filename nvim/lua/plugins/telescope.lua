9467dc58c59618db92bc52938f5350b715f8913e
Dor256 <dorb@wix.com>
2026-03-19 14:48:11 +0200

    replace bufferline with telescope buffer search

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,25 @@
 return {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
     version = "*",
     dependencies = {
-        'nvim-lua/plenary.nvim',
+        "nvim-lua/plenary.nvim",
         -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+        local actions = require("telescope.actions")
+        require("telescope").setup({
+            defaults = {
+                mappings = {
+                    n = {
+                        ["<C-d>"] = actions.delete_buffer
+                    },
+                    i = {
+                        ["<C-h>"] = "which_key",
+                        ["<C-d>"] = actions.delete_buffer,
+                    },
+                },
+            },
+        })
+    end,
 }
-
